6fa238a87f76468db4ffe5686f1d98266f13ac40
Omar Crosby <omar.crosby@gmail.com>
2025-06-24 19:31:48 -0400

    chore: fix lint script

---
+++ express-template/eslint.config.cjs
@@ -0,0 +1,23 @@
+// eslint.config.cjs
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        module: "readonly",
+        require: "readonly",
+        __dirname: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"]
+    }
+  }
+];
--- express-template/src/middleware/errorHandler.js
+++ express-template/src/middleware/errorHandler.js
@@ -2,7 +2,7 @@ const createError = require('http-errors');
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-  // eslint-disable-next-line no-console
+   
   console.error(err);
   // if the error is safe to expose to client
   if (err.expose === true) {
